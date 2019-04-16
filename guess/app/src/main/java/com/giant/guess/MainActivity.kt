91d21eeecee4daae5e7a85bf0c187bba263012dd
prince <yenching203@gmail.com>
2019-04-16 10:56:16 +0800

    function design, dialog

--- guess/app/src/main/java/com/giant/guess/MainActivity.kt
+++ guess/app/src/main/java/com/giant/guess/MainActivity.kt
@@ -2,11 +2,33 @@ package com.giant.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun check(view: View) {
+        val n = ed_number.text.toString().toInt()
+        val diff = secretNumber.validate(n)
+        var message = "Yes, you got it"
+        if (diff > 0) {
+            message = "Smaller"
+        } else if (diff < 0) {
+            message = "Bigger"
+        }
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
